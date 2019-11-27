08be9375f47ac14feef332e8bdbf333a21b98264
wiejak555 <wiejak555@gmail.com>
2019-11-27 21:20:30 +0100

    computerMove randomize

--- js/script.js
+++ js/script.js
@@ -8,7 +8,15 @@ function clearMessages() {
     document.getElementById('messages').innerHTML = '';
 }
 
-var computerMove, playetMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'nozyce';
+} else computerMove = 'nieznany ruch';
+
+printMessage('Mój ruch: ' + computerMove);
