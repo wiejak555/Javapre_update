b70b4741c713f28cfd37defb4827d8f8689892c2
wiejak555 <wiejak555@gmail.com>
2019-11-27 21:32:44 +0100

    players choice

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ if (randomNumber == '1') {
 } else computerMove = 'nieznany ruch';
 
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nozyce';
+} else {
+    playerMove = 'kamien'
+    printMessage("Wybarana zła liczba - poprawione na 1 - czyli kamien");
+}
+printMessage('Twój ruch: ' + playerMove);
